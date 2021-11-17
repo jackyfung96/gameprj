cebe0794995d47f1232215d6455cdbaa5ca8ed65
jackyfung96 <jackii.fung@gmail.com>
2021-11-17 16:26:10 +0000

    15nov

--- test1.py
+++ test1.py
@@ -5,10 +5,11 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()  # Capitalise
-test_font = pygame.font.Font(None, 50)
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png')
 ground = pygame.image.load('graphics/ground.png')
+text_surface = test_font.render('My game', False, 'Black')
 
 while True:
     for event in pygame.event.get():
@@ -18,6 +19,7 @@ while True:
 
     screen.blit(sky_surface, (0, 0))  # blit = block image transfer
     screen.blit(ground, (0, 300))
+    screen.blit(text_surface, (300, 50))
 
     pygame.display.update()
     clock.tick(60)
